deed303623c28bdcb7ed42d98253f0e06aa14412
Jason9789 <lifebook0809@gmail.com>
2022-01-31 14:00:33 +0900

    study: Conditional Rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,14 @@ import ClassComponent from './components/2-5.State/ClassComponent';
 import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent2';
 import Event from './components/2-7.Event/Event'
+import Condition from './components/2-8.ConditionalRendering/Condition';
 
 function App() {
   return (
     <div className="App">
 
-    <Event />
+    <Condition />
+    {/* <Event /> */}
     
     {/* <FunctionalComponent />
       <ClassComponent />
---
+++ my-app/src/components/2-8.ConditionalRendering/Condition.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function UserGreeting(props) {
+  return <h1>{props.name && props.name + ', '} Welcome {props.count ? `It's ${props.count} times` : null}</h1>
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>
+}
+
+
+function Greeting(props) {
+  // if(props.isLoggedIn) {
+  //   return <UserGreeting name='jason' count={0} />
+  // }
+
+  // return <GuestGreeting />
+
+  return props.isLoggedIn ? <UserGreeting name="Jason" count={0} /> : <GuestGreeting />
+}
+
+export default function Condition() {
+
+  const isLoggedIn = true
+
+
+  return <div>
+    <Greeting isLoggedIn={isLoggedIn} />
+  </div>;
+}
