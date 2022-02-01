46dae50baefb0edad307c504f96534fbe872fb39
Jason9789 <lifebook0809@gmail.com>
2022-02-01 14:12:41 +0900

    study: Hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import ControlledComponent from './components/2-10.Form/ControlledComponent';
-import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
+import Reducer from './components/3-3.Hooks/Reducer';
+import State from './components/3-3.Hooks/State';
+// import ControlledComponent from './components/2-10.Form/ControlledComponent';
+// import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
 // import Composition from './components/2-4.Props/Composition';
 // import Extraction from './components/2-4.Props/Extraction/Extraction';
 // import ClassComponent from './components/2-5.State/ClassComponent';
@@ -8,14 +10,17 @@ import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent'
 // import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent2';
 // import Event from './components/2-7.Event/Event'
 // import Condition from './components/2-8.ConditionalRendering/Condition';
-import List from './components/2-9.List/List';
+// import List from './components/2-9.List/List';
 
 function App() {
   return (
     <div className="App">
 
-    <ControlledComponent />
-    <UncontrolledComponent />
+      <State />
+      <Reducer />
+
+    {/* <ControlledComponent />
+    <UncontrolledComponent /> */}
     {/* <List /> */}
     {/* <Condition /> */}
     {/* <Event /> */}
