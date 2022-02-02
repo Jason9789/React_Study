f8d02e8164ca7c16b020630a7baafa38b1e0ada0
Jason9789 <lifebook0809@gmail.com>
2022-02-02 16:08:27 +0900

    study: Composition2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import Dialog from './components/3-5.Composition/Dialog';
+// import Dialog from './components/3-5.Composition/Dialog';
 // import Reducer from './components/3-3.Hooks/Reducer';
 // import State from './components/3-3.Hooks/State';
-import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
+// import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
+// import Dialog2 from './components/3-6.Composition2/Dialog2';
+import ThankYouDialog from './components/3-6.Composition2/ThankYouDialog';
 // import ControlledComponent from './components/2-10.Form/ControlledComponent';
 // import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
 // import Composition from './components/2-4.Props/Composition';
@@ -18,7 +20,10 @@ function App() {
   return (
     <div className="App">
 
-      <WelcomeDialog />
+      <ThankYouDialog />
+      {/* <Dialog2 /> */}
+
+      {/* <WelcomeDialog /> */}
 
       {/* <State />
       <Reducer /> */}
