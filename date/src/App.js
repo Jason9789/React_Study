d2f8d4859c4170802422e6b0f130115063775f8e
Jason9789 <lifebook0809@gmail.com>
2022-02-06 16:17:44 +0900

    study: Dayjs

--- date/src/App.js
+++ date/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import DayjsExample from './components/Dayjs/DayjsExample';
 import MomentExample from './components/Moment/MomentExample';
 
 function App() {
   return (
     <div className="App">
+      <DayjsExample />
       <MomentExample />
+      
     </div>
   );
 }
