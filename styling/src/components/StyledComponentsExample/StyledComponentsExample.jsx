66ca4d67eb3830feb30f144a025fe8254acd0962
Jason9789 <lifebook0809@gmail.com>
2022-02-07 12:28:31 +0900

    study: Styled-Components

--- styling/src/components/StyledComponentsExample/StyledComponentsExample.jsx
+++ styling/src/components/StyledComponentsExample/StyledComponentsExample.jsx
@@ -1,8 +1,98 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 export default function StyledComponentsExample() {
+      const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+
+  const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+  const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+  `;
+
+  const ReversedButton = props => <
+    Button {...props} children={props.children.split('').reverse()} />
+    
+    const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
+    color: blue;
+  
+    &:hover {
+      color: red; // <Thing> when hovered
+    }
+  
+    & ~ & {
+      background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+    }
+  
+    & + & {
+      background: lime; // <Thing> next to <Thing>
+    }
+  
+    &.something {
+      background: orange; // <Thing> tagged with an additional CSS class ".something"
+    }
+  
+    .something-else & {
+      border: 1px solid; // <Thing> inside another element labeled ".something-else"
+    }
+  `
+  // Use Title and Wrapper like any other React component – except they're styled!
   return (
-    <div></div>
+
+    <React.Fragment>
+    <Thing>Hello world!</Thing>
+    <Thing>How ya doing?</Thing>
+    <Thing className="something">The sun is shining...</Thing>
+    <div>Pretty nice day today.</div>
+    <Thing>Don't you think?</Thing>
+    <div className="something-else">
+      <Thing>Splendid.</Thing>
+    </div>
+  </React.Fragment>
+
+
+
+    // <>
+    // <Wrapper>
+    //   <Title>
+    //     Hello World!
+    //   </Title>
+    // </Wrapper>
+
+    // <Button onClick={ () => alert('normal') }>Normal</Button>
+    // <Button primary onClick={ () => alert('primary') }>Primary</Button>
+    // <TomatoButton>Tomato Button</TomatoButton>
+
+    // <br />
+    // <Button>Normal Button</Button>
+    // <Button primary as="a" href="#">Link with Button styles</Button>
+    // <TomatoButton as="a" href="#">Link with Tomato Button styles</TomatoButton>
+
+    // <br />
+    // <Button>Normal Button</Button>
+    // <Button as={ReversedButton}>Custom Button with Normal Button styles</Button>
+
+    // </>
   );
 }
