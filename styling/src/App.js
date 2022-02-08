f1dd65721cc1a3742427527d1038301f5a8eaec2
Jason9789 <lifebook0809@gmail.com>
2022-02-08 13:41:49 +0900

    study: scss

--- styling/src/App.js
+++ styling/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import StyledComponentsExample from './components/StyledComponentsExample/StyledComponentsExample';
 import EmotionExample from './components/EmotionExample/EmotionExample';
--- styling/src/components/SassExample/Example.jsx
+++ styling/src/components/SassExample/Example.jsx
@@ -7,6 +7,33 @@ export default function Example() {
     <p>Example</p>
     <p className="font">Example</p>
 
+    <nav>
+      <ul>
+        <li>123</li>
+        <li>
+          <a>456</a>
+        </li>
+      </ul>
+    </nav>
+
+    <ul>
+        <li>123</li>
+        <li>
+          <a>456</a>
+        </li>
+    </ul>
+
+      <p className="base">Hello</p>
+      <p className="inverse">Hello</p>
+
+      <p className="info">Info</p>
+      <p className="alert">alert</p>
+      <p className="success">success</p>
+
+      <p className="message">message</p>
+      <p className="success2">success2</p>
+      <p className="error">error</p>
+      <p className="warning">warning</p>
     </>
   );
 }
