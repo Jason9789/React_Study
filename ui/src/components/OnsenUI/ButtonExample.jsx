e9ad0d1cd1ca68233058c9c801b12a1926ee0d2d
Jason9789 <lifebook0809@gmail.com>
2022-02-09 14:21:21 +0900

    study: onsen

---
+++ ui/src/components/OnsenUI/ButtonExample.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from 'react-onsenui'
+
+export default function ButtonExample() {
+
+  const handleClick = () => {
+    alert("Pressed")
+  }
+
+  return (
+    <>
+    <Button onClick={ handleClick } modifier="large--cta">
+      Tap Me
+    </Button>
+
+    <br />
+    <br />
+
+    <Button onClick={handleClick} disabled={ true }>
+      Disabled
+    </Button>
+
+    <br />
+    <br />
+
+    <Button onClick={ handleClick } modifier="quite">
+      Quite
+    </Button>
+    </>
+  );
+}
