e327f900e65901d7dd8cdafa8ece5417fabeb1b8
Jason9789 <lifebook0809@gmail.com>
2022-02-13 22:36:55 +0900

    add: first setting

---
+++ oauth-login-test/src/Components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div style={{
+        fontSize: '2rem',
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '2rem',
+      }}
+    >
+      Main
+      </div>
+  )
+}
+
+export default Main
