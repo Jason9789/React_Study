4423d6e4a1f9bceeb04be80d696b6200ca0538d4
Jason9789 <lifebook0809@gmail.com>
2022-02-15 17:17:56 +0900

    hold

---
+++ oauth-login-test/src/Components/Login/GoogleButton.jsx
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react'
+// import { GoogleLogin } from 'react-google-login'
+// import styled from 'styled-components'
+
+// const Container = styled.div`
+// display: flex;
+// flex-flow: column wrap;
+// `
+
+// const clientId = ""
+
+// export default function GoogleButton() {
+
+//   return (
+//     <Container>
+//        <GoogleButton />
+//      </Container>
+//   )
+// }
--- oauth-login-test/src/Components/Login/Login.jsx
+++ oauth-login-test/src/Components/Login/Login.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Logo from '../Template/Logo'
 import Template from '../Template/Template'
+// import GoogleButton from './GoogleButton'
 
 export default function Login() {
   return (
     <>
       <Template>
         <Logo />
+        {/* <GoogleButton /> */}
       </Template>
     </>
 
