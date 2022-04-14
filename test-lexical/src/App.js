c05f88d6632eef248cc9cd6e424a90739d772da8
Jason9789 <lifebook0809@gmail.com>
2022-04-14 17:04:33 +0900

    Lexical Test

---
+++ test-lexical/src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+import './styles.css';
+import Editor from './Editor';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Plain Text Example</h1>
+      <p>Note: this is an experimental build of Lexical</p>
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
