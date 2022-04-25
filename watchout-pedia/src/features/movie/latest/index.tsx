89d101f20ed1134cce2058223546bd8a119e229c
Jason9789 <lifebook0809@gmail.com>
2022-04-25 17:10:16 +0900

    hold

---
+++ watchout-pedia/src/features/movie/latest/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Base = styled.div``
+
+const Title = styled.h4``
+
+const LatestMovieSection: React.FC = () => {
+	return (
+		<Base>
+			<Title>최근 개봉작</Title>
+		</Base>
+	)
+}
+
+export default LatestMovieSection
