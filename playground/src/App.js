b4d964fd640e52362d37bc35f0e39101dd9febf5
Jason9789 <lifebook0809@gmail.com>
2022-05-31 15:19:05 +0900

    study: Element

---
+++ playground/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ElementRender from './components/ElementRender';
+import Name from './components/Name';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello, World!</h1>
+      <Name />
+      <ElementRender />
+    </div>
+  );
+}
+
+export default App;
---
+++ playground/src/components/Name.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function name() {
+  const name = 'Jason';
+
+  return <div>{name}</div>;
+}
