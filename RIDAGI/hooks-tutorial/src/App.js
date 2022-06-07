d74966229cb4f2d73801ac79067251bc15f084e8
Jason9789 <lifebook0809@gmail.com>
2022-06-07 16:00:54 +0900

    study: Chapter 8: useState, useEffect, useReducer

--- RIDAGI/hooks-tutorial/src/App.js
+++ RIDAGI/hooks-tutorial/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Counter from './Components/Counter';
+
+import Info from './Components/Info';
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Counter />
+
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
     </div>
   );
 }
