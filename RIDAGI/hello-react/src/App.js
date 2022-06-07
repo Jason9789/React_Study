80b13b899d3f6791c95f062f8825c1f72b367ea5
Jason9789 <lifebook0809@gmail.com>
2022-06-07 13:51:21 +0900

    study: Chapter 7 LifeCycle

--- RIDAGI/hello-react/src/App.js
+++ RIDAGI/hello-react/src/App.js
@@ -1,21 +1,30 @@
 import './App.css'
-import ValidationSample from './components/ValidationSample'
-
 import React, { Component } from 'react'
-import ScrollBox from './components/ScrollBox'
-import IterationSample from './components/IterationSample'
+import LifeCycleSample from './components/LifeCycleSample'
+import ErrorBoundary from './components/ErrorBoundary'
 
+// 랜덤 색상 정하기
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
 export default class App extends Component {
+  state = {
+    color: '#000000',
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    })
+  }
+
   render() {
     return (
       <div>
-        <ValidationSample />
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     )
   }
