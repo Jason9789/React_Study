59dc4f4346a937eb88aeee0bd8a561062e73fd49
Jason9789 <lifebook0809@gmail.com>
2022-06-07 11:48:04 +0900

    study: Chapter 6 Map, Filter

--- RIDAGI/hello-react/src/App.js
+++ RIDAGI/hello-react/src/App.js
@@ -3,6 +3,7 @@ import ValidationSample from './components/ValidationSample'
 
 import React, { Component } from 'react'
 import ScrollBox from './components/ScrollBox'
+import IterationSample from './components/IterationSample'
 
 export default class App extends Component {
   render() {
@@ -13,6 +14,8 @@ export default class App extends Component {
         <button onClick={() => this.scrollBox.scrollToBottom()}>
           맨 밑으로
         </button>
+
+        <IterationSample />
       </div>
     )
   }
