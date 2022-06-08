24fec7baf52a1809d30fc1736fa05fd334ec073e
Jason9789 <lifebook0809@gmail.com>
2022-06-08 18:37:52 +0900

    add: styled-components

--- RIDAGI/styling-react/src/App.js
+++ RIDAGI/styling-react/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import CSSModule from './Components/CSSModule';
 import SassComponent from './Components/SassComponent';
+import StyledComponent from './Components/StyledComponent';
 
 function App() {
   return (
     <div>
       {/* <SassComponent /> */}
-      <CSSModule />
+      {/* <CSSModule /> */}
+      <StyledComponent />
     </div>
   );
 }
