bd34e9c170674a8f34faab9c56836ebb283703fc
Jason9789 <lifebook0809@gmail.com>
2022-06-08 17:51:51 +0900

    init: project

---
+++ RIDAGI/styling-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SassComponent from './Components/SassComponent';
+
+function App() {
+  return (
+    <div className='App'>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
