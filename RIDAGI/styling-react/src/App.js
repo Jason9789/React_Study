ea188b05862f4349fb23113af94ca22f67b76e32
Jason9789 <lifebook0809@gmail.com>
2022-06-08 18:16:20 +0900

    add: CSS Module

--- RIDAGI/styling-react/src/App.js
+++ RIDAGI/styling-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CSSModule from './Components/CSSModule';
 import SassComponent from './Components/SassComponent';
 
 function App() {
   return (
-    <div className='App'>
-      <SassComponent />
+    <div>
+      {/* <SassComponent /> */}
+      <CSSModule />
     </div>
   );
 }
