58886709902b42692fea104dd768844f802a43b4
Jason9789 <lifebook0809@gmail.com>
2022-06-14 15:12:03 +0900

    test: d3-geo

---
+++ d3-geo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MapChart from './components/Map';
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <h1>D3 GEO Tutorial</h1>
+        <MapChart />
+      </div>
+    </div>
+  );
+}
+
+export default App;
