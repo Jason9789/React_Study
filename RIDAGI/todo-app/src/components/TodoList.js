7d02b668086ffa85210ffa3c490f157c1b38bc5e
Jason9789 <lifebook0809@gmail.com>
2022-06-15 16:51:36 +0900

    add: react-virtualized

--- RIDAGI/todo-app/src/components/TodoList.js
+++ RIDAGI/todo-app/src/components/TodoList.js
@@ -1,20 +1,36 @@
-import React from 'react'
-import './TodoList.scss'
+import React, { useCallback } from 'react'
+import { List } from 'react-virtualized'
 import TodoListItem from './TodoListItem'
+import './TodoList.scss'
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRerenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index]
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      )
+    },
+    [onRemove, onToggle, todos],
+  )
+  return (
+    <List
+      className="TodoList"
+      width={512} // 전체 크기
+      height={513} // 전체 높이
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRerenderer}
+      list={todos} // 배열
+      style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
+    />
   )
 }
 
-export default TodoList
+export default React.memo(TodoList)
