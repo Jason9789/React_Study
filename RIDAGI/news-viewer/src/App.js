91df5e2cf9e8ff2c54c8f36a4025015446cdce27
Jason9789 <lifebook0809@gmail.com>
2022-06-16 15:42:42 +0900

    add: News list with styled

--- RIDAGI/news-viewer/src/App.js
+++ RIDAGI/news-viewer/src/App.js
@@ -1,36 +1,8 @@
-import './App.css'
-import axios from 'axios'
-import { useState } from 'react'
+// import './App.css'
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null)
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=9d53eaef21a1426f86e09acfd21bd799',
-      )
-      setData(response.data)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  return (
-    <div className="App">
-      <h1>News Viewer</h1>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  )
+  return <NewsList />;
 }
 
-export default App
+export default App;
