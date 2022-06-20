473df2c54b55f18de293f942af63a72fc6694403
Jason9789 <lifebook0809@gmail.com>
2022-06-20 19:28:09 +0900

    add: redux-actions, immer

--- RIDAGI/react-redux-tutorial/src/modules/counter.js
+++ RIDAGI/react-redux-tutorial/src/modules/counter.js
@@ -1,32 +1,28 @@
+import { createAction, handleActions } from 'redux-actions'
+
 // 1. 가장 먼저 할 작업은 액션 타입을 정의하는 것이다.
 // "모듈 이름/액션 이름"과 같은 형태로 액션 타입을 정의한다.
 const INCREASE = 'counter/INCREASE'
 const DECREASE = 'counter/DECREASE'
 
-// 액션 함수 만들기
-export const increase = () => ({ type: INCREASE })
-export const decrease = () => ({ type: DECREASE })
+// 액션 함수 만들기 -> createAction을 사용하면 더 편하게 가능
+export const increase = createAction(INCREASE)
+export const decrease = createAction(DECREASE)
 
 // 초기 상태 및 리듀서 함수 만들기
 const initialState = {
   number: 0,
 }
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      }
-
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      }
-
-    default:
-      return state
-  }
-}
+// 리듀서 함수도 handleActions를 사용하면 더 간단하고 가독성 높게 작성할 수 있다.
+// 첫 번째 파라미터에는 각 액션에 대한 업데이트 함수
+// 두 번째 파라미터에는 초기 상태
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+)
 
 export default counter
--- RIDAGI/react-redux-tutorial/src/modules/todos.js
+++ RIDAGI/react-redux-tutorial/src/modules/todos.js
@@ -1,33 +1,23 @@
+import produce from 'immer'
+import { createAction, handleActions } from 'redux-actions'
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'
 const INSERT = 'todos/INSERT'
 const TOGGLE = 'todos/TOGGLE'
 const REMOVE = 'todos/REMOVE'
 
 // 액션 함수
-export const changeInput = (input) => ({
-  type: CHANGE_INPUT,
-  input,
-})
+export const changeInput = createAction(CHANGE_INPUT, (input) => input)
 
 let id = 3 // insert가 호출될 때마다 1씩 더해짐
-export const insert = (text) => ({
-  type: INSERT,
-  todo: {
-    id: id++,
-    text,
-    done: false,
-  },
-})
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+}))
 
-export const toggle = (id) => ({
-  type: TOGGLE,
-  id,
-})
-
-export const remove = (id) => ({
-  type: REMOVE,
-  id,
-})
+export const toggle = createAction(TOGGLE, (id) => id)
+export const remove = createAction(REMOVE, (id) => id)
 
 // 초기 상태 및 리듀서 함수 만들기
 const initialState = {
@@ -46,39 +36,29 @@ const initialState = {
   ],
 }
 
-function todos(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      }
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input
+      }),
 
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo),
-      }
-
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.id
-            ? { ...todos, id: todo.id, text: todo.text, done: !todo.done }
-            : todo,
-        ),
-      }
-
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      }
-
-    default:
-      return state
-  }
-}
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo)
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id)
+        todo.done = !todo.done
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id)
+        draft.todos.splice(index, 1)
+      }),
+  },
+  initialState,
+)
 
 export default todos
